73dd5eaab6d8f81e5d221fa7a85906b745ec0fec
Peng Guo <aaron@pengg-mac.local>
2019-07-12 17:35:26 +0800

    fix package name

--- presto-ranger-systemaccesscontrol/src/main/java/sg/bigo/plugin/ranger/PrincipalUserMatchRule.java
+++ presto-ranger-systemaccesscontrol/src/main/java/sg/bigo/plugin/ranger/PrincipalUserMatchRule.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sg.bigo.presto.hive.parse;
+package sg.bigo.plugin.ranger;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
--- presto-ranger-systemaccesscontrol/src/main/java/sg/bigo/plugin/ranger/RangerSystemAccessControlPlugin.java
+++ presto-ranger-systemaccesscontrol/src/main/java/sg/bigo/plugin/ranger/RangerSystemAccessControlPlugin.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sg.bigo.presto.hive.parse;
+package sg.bigo.plugin.ranger;
 
 import com.google.common.collect.ImmutableList;
 import io.prestosql.spi.Plugin;

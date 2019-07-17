6b2d4ad7bcc954eeff1c560610a94e8fb586d76b
Peng Guo <aaron@pengg-mac.local>
2019-07-17 11:49:07 +0800

    add string type

--- presto-main/src/main/java/io/prestosql/type/TypeRegistry.java
+++ presto-main/src/main/java/io/prestosql/type/TypeRegistry.java
@@ -74,6 +74,7 @@ import static io.prestosql.spi.type.TimestampType.TIMESTAMP;
 import static io.prestosql.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
 import static io.prestosql.spi.type.TinyintType.TINYINT;
 import static io.prestosql.spi.type.VarbinaryType.VARBINARY;
+import static io.prestosql.spi.type.VarcharType.VARCHAR;
 import static io.prestosql.spi.type.VarcharType.createUnboundedVarcharType;
 import static io.prestosql.spi.type.VarcharType.createVarcharType;
 import static io.prestosql.type.ArrayParametricType.ARRAY;
@@ -118,6 +119,9 @@ public final class TypeRegistry
     {
         requireNonNull(types, "types is null");
         this.featuresConfig = requireNonNull(featuresConfig, "featuresConfig is null");
+
+        // this is hack here, we are trying to give varchar type a different name called string
+        this.types.putIfAbsent(new TypeSignature("string"), VARCHAR);
 
         // Manually register UNKNOWN type without a verifyTypeClass call since it is a special type that can not be used by functions
         this.types.put(UNKNOWN.getTypeSignature(), UNKNOWN);

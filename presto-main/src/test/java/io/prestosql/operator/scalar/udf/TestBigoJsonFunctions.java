021dbf62eca58ca9e0ac917a666da13f4adffefa
jibaohua <jibaohua@bigo.sg>
2019-07-30 16:19:42 +0800

    testGetJsonObject

--- presto-main/src/test/java/io/prestosql/operator/scalar/udf/TestBigoJsonFunctions.java
+++ presto-main/src/test/java/io/prestosql/operator/scalar/udf/TestBigoJsonFunctions.java
@@ -1,18 +1,19 @@
-package io.prestosql.operator.scalar;
+package io.prestosql.operator.scalar.udf;
 
 import io.airlift.slice.Slice;
-import io.prestosql.operator.scalar.udf.BigoJsonFunctions;
+import io.prestosql.operator.scalar.JsonPath;
 import org.testng.annotations.Test;
 
 import static io.airlift.slice.Slices.utf8Slice;
+import static io.prestosql.operator.scalar.udf.BigoJsonFunctions.varcharGetJsonObject;
 import static org.testng.Assert.*;
 
 public class TestBigoJsonFunctions {
 
     @Test
-    public void testvarcharGetJsonObject()
+    public void testGetJsonObject()
     {
-        Slice slice = BigoJsonFunctions.varcharGetJsonObject(utf8Slice("[1, 2, 3]"), new JsonPath("$[2]"));
+        Slice slice = varcharGetJsonObject(utf8Slice("[1, 2, 3]"), new JsonPath("$[2]"));
         assertEquals(slice.toStringUtf8(), "3");
     }
 }

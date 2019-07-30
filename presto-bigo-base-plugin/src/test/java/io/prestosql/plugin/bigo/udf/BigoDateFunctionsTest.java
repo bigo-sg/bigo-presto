1bdc08cc4e6951683ebc7e80dbc11d55724cec77
jibaohua <jibaohua@bigo.sg>
2019-07-30 21:11:01 +0800

    udf date_diff

--- presto-bigo-base-plugin/src/test/java/io/prestosql/plugin/bigo/udf/BigoDateFunctionsTest.java
+++ presto-bigo-base-plugin/src/test/java/io/prestosql/plugin/bigo/udf/BigoDateFunctionsTest.java
@@ -3,6 +3,8 @@ package io.prestosql.plugin.bigo.udf;
 import io.prestosql.spi.PrestoException;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
+
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.prestosql.plugin.bigo.udf.BigoDateFunctions.unixTimestamp;
 import static org.testng.Assert.*;
@@ -38,13 +40,30 @@ public class BigoDateFunctionsTest {
     public void testUnixTimestamp()
     {
         double uts = unixTimestamp(utf8Slice("2019-07-22 00:00:00"));
-        assertEquals(uts, 1563724800);
+        if(String.valueOf(uts).contains("E")){
+            BigDecimal bd1 = new BigDecimal(uts);
+            assertEquals(bd1.toPlainString(), "1563724800");
+        }else {
+            assertEquals(String.valueOf(uts), "1563724800");
+        }
     }
 
     @Test
     public void testUnixTimestampWithFormat()
     {
         double uts = unixTimestamp(utf8Slice("2019-07-22"), utf8Slice("yyyy-MM-dd"));
-        assertEquals(uts, 1563724800);
+        if(String.valueOf(uts).contains("E")){
+            BigDecimal bd1 = new BigDecimal(uts);
+            assertEquals(bd1.toPlainString(), "1563724800");
+        }else {
+            assertEquals(String.valueOf(uts), "1563724800");
+        }
+    }
+
+    @Test
+    public void testDateDiff()
+    {
+        String diff = BigoDateFunctions.dateDiff(utf8Slice("2019-01-03"), utf8Slice("2018-12-31")).toStringUtf8();
+        assertEquals(diff, "3");
     }
 }

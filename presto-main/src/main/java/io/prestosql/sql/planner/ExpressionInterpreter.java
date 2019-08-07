e56921e9c7391af024fa9cc08e8988b857262676
Peng Guo <gpcuster@gmail.com>
2019-08-07 15:23:26 +0800

    allow array subscript operator behaviour differently on hive and presto

--- presto-main/src/main/java/io/prestosql/sql/planner/ExpressionInterpreter.java
+++ presto-main/src/main/java/io/prestosql/sql/planner/ExpressionInterpreter.java
@@ -1231,7 +1231,7 @@ public class ExpressionInterpreter
                 return null;
             }
             if ((index instanceof Long) && isArray(type(node.getBase()))) {
-                ArraySubscriptOperator.checkArrayIndex((Long) index);
+                ArraySubscriptOperator.checkArrayIndex(session, (Long) index);
             }
 
             if (hasUnresolvedValue(base, index)) {

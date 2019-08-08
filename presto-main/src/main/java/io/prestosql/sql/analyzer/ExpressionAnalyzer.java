042e21d61e5ed368a5e1694a3d33b7a4c58f876d
jibaohua <jibaohua@bigo.sg>
2019-08-08 20:12:03 +0800

    implicit type conversion for arithmetic binary operators, between...and...

--- presto-main/src/main/java/io/prestosql/sql/analyzer/ExpressionAnalyzer.java
+++ presto-main/src/main/java/io/prestosql/sql/analyzer/ExpressionAnalyzer.java
@@ -1125,7 +1125,10 @@ public class ExpressionAnalyzer
                 Type valueType = process(valueExpression, context);
                 Type maxType = process(maxExpression, context);
 
-                if(minType == null || valueType == null || maxType == null) {
+                if (minType == null || valueType == null || maxType == null) {
+                    return getOperator(context, node, OperatorType.BETWEEN, valueExpression, minExpression, maxExpression);
+                }
+                if (tc.compare3TypesOrder(minType, valueType, maxType) == null) {
                     return getOperator(context, node, OperatorType.BETWEEN, valueExpression, minExpression, maxExpression);
                 }
 

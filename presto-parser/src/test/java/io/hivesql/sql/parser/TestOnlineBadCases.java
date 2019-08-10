9be74a8855059bff58926c6cd74acfa44bb888c3
tangyun <tangyun@bigo.sg>
2019-08-10 18:06:47 +0800

    add some test case & improve create table complex types

---
+++ presto-parser/src/test/java/io/hivesql/sql/parser/TestOnlineBadCases.java
@@ -0,0 +1,23 @@
+package io.hivesql.sql.parser;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestOnlineBadCases extends SQLTester {
+
+    @Test
+    public void testCase01() {
+        checkASTNodeFromFile("hive/parser/cases/plan-timeout.sql");
+    }
+
+    @Test
+    public void testCase02() {
+        try {
+            runHiveSQLFromFile("hive/parser/cases/online-case-1.sql");
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), "line 1:1: please check, how should we merge them? " +
+                    "most possible reason is executing a syntax that hive not support");
+        }
+    }
+
+}

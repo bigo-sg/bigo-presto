7730c1a47bf15666167b32d1f7638bee3d5a17cf
Tangyun <tangyun@bigo.sg>
2019-08-15 14:23:30 +0800

    support desc table

---
+++ presto-parser/src/test/java/io/hivesql/sql/parser/TestDesc.java
@@ -0,0 +1,30 @@
+package io.hivesql.sql.parser;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author tangyun@bigo.sg
+ * @date 8/15/19 2:12 PM
+ */
+public class TestDesc extends SQLTester {
+
+    @Test
+    public void test01() {
+        String sql = "desc tablename";
+        checkASTNode(sql);
+    }
+    @Test
+    public void test02() {
+        String hiveSql = "desc a.tablename";
+        String prestoSql = "desc a.tablename";
+        checkASTNode(prestoSql, hiveSql);
+    }
+
+    @Test
+    public void test03() {
+        String hiveSql = "desc table a.tablename";
+        String prestoSql = "desc a.tablename";
+        checkASTNode(prestoSql, hiveSql);
+    }
+
+}

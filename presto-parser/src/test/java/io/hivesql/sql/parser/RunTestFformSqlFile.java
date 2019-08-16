10a1ea289a38442f44d7c218b7f2d3c75c6fcd08
Tangyun <tangyun@bigo.sg>
2019-08-16 16:29:08 +0800

    support columns and tableName start with number

--- presto-parser/src/test/java/io/hivesql/sql/parser/RunTestFformSqlFile.java
+++ presto-parser/src/test/java/io/hivesql/sql/parser/RunTestFformSqlFile.java
@@ -26,25 +26,6 @@ public class RunTestFformSqlFile extends SQLTester {
     }
 
     @Test
-    public void test01() {
-        String rootPath = this.getClass().getResource("../../../../").getFile();
-        System.out.println(rootPath);
-        readAndTestFile(rootPath +  "hive/parser/r1.sql");
-    }
-
-    @Test
-    public void test02() {
-        String rootPath = this.getClass().getResource("../../../../").getFile();
-        System.out.println(rootPath);
-
-        byte[] data = FileUtils.getFileAsBytes(rootPath +  "hive/parser/pass/pass6.sql");
-        String sqls = new String(data);
-        System.out.println(sqls);
-        readAndRunSqlFile(rootPath +  "hive/parser/pass/pass6.sql");
-
-    }
-
-    @Test
     public void testByScanDir() {
         String rootPath = this.getClass().getResource("../../../../hive/parser/compare").getFile();
         List<String> sqlFiles = FileUtils.scanDirectory(rootPath);
--- presto-parser/src/test/resources/hive/parser/cases/lateral-view-hive-5.sql
+++
@@ -1,5 +0,0 @@
-select i.uids, u from bigolive.bigo_online_user_uversion_stat_platform
-lateral view explode(online) o as i
-lateral view explode(i.uids) uids as u
-where day='2019-08-08'
-limit 10

1f405675af34b4a94932b14d537e4113ebb79f70
jibaohua <jibaohua@bigo.sg>
2019-08-19 20:20:50 +0800

    int divided by int, returns double in hive mode. add hex, unhex, ascii, array_contains function. add shiftright, shiftrightunsigned, shiftleft and pmod function. add int, double, string function.

---
+++ presto-bigo-base-plugin/src/main/java/io/prestosql/plugin/bigo/udf/ShiftRightUnsignedFunction.java
@@ -0,0 +1,52 @@
+package io.prestosql.plugin.bigo.udf;
+
+import io.airlift.slice.Slice;
+import io.prestosql.spi.function.Description;
+import io.prestosql.spi.function.ScalarFunction;
+import io.prestosql.spi.function.SqlNullable;
+import io.prestosql.spi.function.SqlType;
+import io.prestosql.spi.type.StandardTypes;
+
+public class ShiftRightUnsignedFunction {
+
+    @Description("Shifts a b positions to the right: shiftright(a,b)")
+    @ScalarFunction("shiftrightunsigned")
+    @SqlType(StandardTypes.BIGINT)
+    @SqlNullable
+    public static Long shiftRightInt(@SqlType(StandardTypes.INTEGER) long left,
+                                     @SqlType(StandardTypes.INTEGER) long right)
+    {
+        return (long)((int)left >>> (int)right);
+    }
+
+    @Description("Shifts a b positions to the right: shiftright(a,b)")
+    @ScalarFunction("shiftrightunsigned")
+    @SqlType(StandardTypes.BIGINT)
+    @SqlNullable
+    public static Long shiftRightSmallInt(@SqlType(StandardTypes.SMALLINT) long left,
+                                          @SqlType(StandardTypes.INTEGER) long right)
+    {
+        return (long)((int)left >>> (int)right);
+    }
+
+    @Description("Shifts a b positions to the right: shiftright(a,b)")
+    @ScalarFunction("shiftrightunsigned")
+    @SqlType(StandardTypes.BIGINT)
+    @SqlNullable
+    public static Long shiftRightTinyInt(@SqlType(StandardTypes.TINYINT) long left,
+                                          @SqlType(StandardTypes.INTEGER) long right)
+    {
+        return (long)((int)left >>> (int)right);
+    }
+
+    @Description("Shifts a b positions to the right: shiftright(a,b)")
+    @ScalarFunction("shiftrightunsigned")
+    @SqlType(StandardTypes.BIGINT)
+    @SqlNullable
+    public static Long shiftRightLongInt(@SqlType(StandardTypes.BIGINT) long left,
+                                      @SqlType(StandardTypes.INTEGER) long right)
+    {
+        return (left >>> right);
+    }
+
+}

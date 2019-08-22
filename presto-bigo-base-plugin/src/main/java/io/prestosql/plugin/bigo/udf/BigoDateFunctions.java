2d7b552ee8edeefbd34360ea1ca658785cd70057
jibaohua <jibaohua@bigo.sg>
2019-08-22 18:24:16 +0800

    fix to_date bug

--- presto-bigo-base-plugin/src/main/java/io/prestosql/plugin/bigo/udf/BigoDateFunctions.java
+++ presto-bigo-base-plugin/src/main/java/io/prestosql/plugin/bigo/udf/BigoDateFunctions.java
@@ -316,9 +316,14 @@ public class BigoDateFunctions {
     @SqlType(StandardTypes.VARCHAR)
     public static Slice stringToDate(@SqlType(StandardTypes.VARCHAR) Slice inputTimestamp)
     {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd[ HH:mm:ss[.SSS]][ zzz]");
-        LocalDate date = LocalDate.parse(inputTimestamp.toStringUtf8(), formatter);
-        return utf8Slice(date.toString());
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            java.sql.Date date = new java.sql.Date(0);
+            Date parsedVal = formatter.parse(inputTimestamp.toStringUtf8());
+            date.setTime(parsedVal.getTime());
+            return utf8Slice(date.toString());
+        } catch (ParseException e) {
+            return null;
+        }
     }
-
 }

23294d14a0c6381451170fb9a654da7111e258f8
Peng Guo <aaron@pengg-mac.local>
2019-12-16 10:32:26 +0800

    execute DDL commands on coordinator

--- presto-main/src/main/java/io/prestosql/server/CoordinatorModule.java
+++ presto-main/src/main/java/io/prestosql/server/CoordinatorModule.java
@@ -82,6 +82,8 @@ import io.prestosql.execution.SetPathTask;
 import io.prestosql.execution.SetRoleTask;
 import io.prestosql.execution.SetSessionTask;
 import io.prestosql.execution.SetHiveConfigurationTask;
+import io.prestosql.execution.ShowColumnsTask;
+import io.prestosql.execution.ShowTablesTask;
 import io.prestosql.execution.SqlQueryManager;
 import io.prestosql.execution.StartTransactionTask;
 import io.prestosql.execution.TaskInfo;
@@ -142,6 +144,8 @@ import io.prestosql.sql.tree.Rollback;
 import io.prestosql.sql.tree.SetPath;
 import io.prestosql.sql.tree.SetRole;
 import io.prestosql.sql.tree.SetSession;
+import io.prestosql.sql.tree.ShowColumns;
+import io.prestosql.sql.tree.ShowTables;
 import io.prestosql.sql.tree.StartTransaction;
 import io.prestosql.sql.tree.Statement;
 import io.prestosql.sql.tree.Use;
@@ -352,6 +356,10 @@ public class CoordinatorModule
         bindDataDefinitionTask(binder, executionBinder, SetHiveConfiguration.class, SetHiveConfigurationTask.class);
         bindDataDefinitionTask(binder, executionBinder, AddManageResource.class, AddManageResourceTask.class);
         bindDataDefinitionTask(binder, executionBinder, CreateFunction.class, CreateFunctionTask.class);
+
+        // run DDL task on coordinator node rather than worker node
+        bindDataDefinitionTask(binder, executionBinder, ShowTables.class, ShowTablesTask.class);
+        bindDataDefinitionTask(binder, executionBinder, ShowColumns.class, ShowColumnsTask.class);
 
         MapBinder<String, ExecutionPolicy> executionPolicyBinder = newMapBinder(binder, String.class, ExecutionPolicy.class);
         executionPolicyBinder.addBinding("all-at-once").to(AllAtOnceExecutionPolicy.class);

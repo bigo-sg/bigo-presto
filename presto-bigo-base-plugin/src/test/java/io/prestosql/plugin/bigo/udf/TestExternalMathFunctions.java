4dc60840b9dcf6d43b8c19d5b45b9fc80b03077e
JiamingMai <jiamingmai@163.com>
2020-03-12 14:12:33 +0800

    added conv udf

---
+++ presto-bigo-base-plugin/src/test/java/io/prestosql/plugin/bigo/udf/TestExternalMathFunctions.java
@@ -0,0 +1,14 @@
+package io.prestosql.plugin.bigo.udf;
+
+import io.prestosql.plugin.bigo.FunctionsTestUtil;
+import io.prestosql.spi.type.VarcharType;
+import org.testng.annotations.Test;
+
+public class TestExternalMathFunctions extends FunctionsTestUtil
+{
+    @Test
+    public void testConv() {
+        assertFunction("conv('2e', 16, 10)", VarcharType.VARCHAR, "46");
+    }
+
+}
